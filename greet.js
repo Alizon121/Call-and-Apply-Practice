5badf26d08ba873d5c9045d83ac3e1b584d4ae06
Andrew <andrew.lizon12@gmail.com>
2024-04-29 21:21:47 -0700

    Completed Phase I

--- greet.js
+++ greet.js
@@ -6,5 +6,7 @@ function say(name) {
 helloMessage = { message: 'Hello,' };
 heyThereMessage = { message: 'Hey there,' };
 
-say(helloMessage, "John");
-say(heyThereMessage, "Michael");
+say.call(helloMessage, "John");
+say.call(heyThereMessage, "Michael");
+
+// reference a function, then follow with 'call' method and pass in an object and arguments
